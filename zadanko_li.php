ce56d315bb5197340a02525010fb2e4289f2dcf5
Michał Małecki <mim7776@wp.pl>
2020-09-16 14:20:23 +0200

    Update zadanko_li.php

--- zadanko_li.php
+++ zadanko_li.php
@@ -1,4 +1,4 @@
-<?php $tab = array(
+<?php $tab1 = array(
     array('id' => 1, 'parent' => 0, 'osoba' => 'pierwszy'),
     array('id' => 2, 'parent' => 1, 'osoba' => array('imie'=>'Adam', 'nazwisko'=>'Mickiewicz')),
     array('id' => 3, 'parent' => 1, 'osoba' => 'trzeci'),
@@ -6,7 +6,32 @@
     array('id' => 5, 'parent' => 1, 'osoba' => array('imie'=>'Leon', 'nazwisko'=>'Wichura')),
     array('id' => 6, 'parent' => 1, 'osoba' => 'szósty')
 );
-$i=0 ?>
+$i=0; 
+$tab = array(
+    array('id' => 1, 'parent' => 0, 'osoba' => 'pierwszy'),
+    array('id' => 2, 'parent' => 1, 'osoba' => array('imie'=>'Adam', 'nazwisko'=>'Mickiewicz', 'zawód'=>array('wyuczony'=>'nauczyciel', 'wykonywany'=>'poeta'))),
+    array('id' => 3, 'parent' => 1, 'osoba' => 'trzeci'),
+    array('id' => 4, 'parent' => 1, 'osoba' => 'czwarty'),
+    array('id' => 5, 'parent' => 1, 'osoba' => array('imie'=>'Leon', 'nazwisko'=>'Wichura')),
+    array('id' => 6, 'parent' => 1, 'osoba' => 'szósty')
+);
+
+function draw_array_in_array($table, $n){
+    if(is_array($table)){
+        $margin = $n*20;
+        $n += 1;
+        foreach($table as $key => $value){  
+            $marginpx = strval($margin)."px";
+            echo "<dd style=\"margin-left:$marginpx\">$key: ";
+            
+            draw_array_in_array($value, $n);
+            }
+        }else{
+            echo "$table</dd>";
+        }
+}
+?>
+
 
 <!DOCTYPE html>
 <html lang="en">
@@ -18,17 +43,9 @@ $i=0 ?>
 <body>
 <dl>
     <?php foreach($tab as $data): ?>
-        <dt><?php echo $i; $i++; ?>:</dt>
-            <?php foreach($data as $key => $value): ?>               
-                <?php if(is_array($value)): ?>
-                    <dd><?php echo $key; ?>: </dd>
-                    <?php foreach($value as $key_2 => $value_2):?>
-                        <dd style="margin-left: 60px"><?php echo $key_2?>: <?php echo $value_2; ?></dd>
-                    <?php endforeach ?>
-                <?php else:?>
-                    <dd><?php echo $key; ?>: <?php echo $value;?></dd>
-                <?php endif ?>
-            <?php endforeach ?>
+        <dt><?php echo $i; $i++; ?>:</dt>              
+                <?php draw_array_in_array($data, 1)?>
+
     <?php endforeach ?>
 </dl>
         
